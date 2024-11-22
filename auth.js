6fbee0c9ed234d644ca54b340b4ce8b71c9ba07a
Stephan Maringka <52527428+stephanmaringka@users.noreply.github.com>
2024-11-22 21:54:10 +0800

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    description: { type: String },
+    imageUrl: { type: String, required: true }
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = Product;
